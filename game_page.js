56a612bc1e95ea112cfda0d3b258a6b5d5d3840d
yash5102008 <75563395+yash5102008@users.noreply.github.com>
2021-08-18 11:38:16 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,14 @@
+player1_name = localstorage.getItem("player1_name")
+player2_name = localStorage.getItem("player2_name");
+
+	player1_score = 0;
+	player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score ;
+document.getElementById("player2_score").innerHTML = player2_score ;
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
